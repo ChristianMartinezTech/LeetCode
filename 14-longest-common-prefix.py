d18120010e0c4faaee8d91a6a7caf27c9cfcf3ef
ChristianMartinezTech <crismartine.e@gmail.com>
2023-03-04 01:21:49 -0500

    Coded 14

---
+++ 14-longest-common-prefix.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+
+"""
+Write a function to find the longest common prefix string amongst an array of strings.
+If there is no common prefix, return an empty string "".
+
+Example 1:
+Input: strs = ["flower","flow","flight"]
+Output: "fl"
+
+Example 2:
+Input: strs = ["dog","racecar","car"]
+Output: ""
+Explanation: There is no common prefix among the input strings.
+ 
+Constraints:
+1 <= strs.length <= 200
+0 <= strs[i].length <= 200
+strs[i] consists of only lowercase English letters.
+"""
+
+def longestCommonPrefix(self, strs):
+    match = strs[0]
+ 
+    for word in strs[1:]:
+        while word[:len(match)] != match:
+            match = match[:-1]
+
+            if not match:
+                return '' 
+
+    return match
